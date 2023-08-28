aa9000debb8b5a01fe2af249b9ce1114c057c4ac
Reed White <RWhite@Carroll.edu>
2023-08-27 20:27:30 -0600

    added more accurate naming and added sub packages

--- src/main/java/edu/carroll/cs389/web/controller/Cs389Application.java
+++ src/main/java/edu/carroll/cs389/web/controller/Cs389Application.java
@@ -1,4 +1,4 @@
-package edu.carroll.cs389;
+package edu.carroll.cs389.web.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/edu/carroll/cs389/web/controller/IndexControllerTest.java
+++ src/test/java/edu/carroll/cs389/web/controller/IndexControllerTest.java
@@ -1,6 +1,7 @@
-package edu.carroll.cs389;
+package edu.carroll.cs389.web.controller;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.containsString;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -11,8 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HelloController.class)
-public class HelloControllerTest {
+@WebMvcTest(IndexController.class)
+public class IndexControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
@@ -20,6 +21,7 @@ public class HelloControllerTest {
     public void indexTest() throws Exception {
         mockMvc.perform(get("/")).andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("index")));
+                .andExpect(content().string(containsString("Welcome to the application!")))
+                .andExpect(content().string(containsString("login")));
     }
 }
